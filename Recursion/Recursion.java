9e542d13e0f1f2ccdce3f0e925b3d1305481c3a0
chetan1821 <cp7292367@gmail.com>
2024-07-22 19:43:49 +0530

    print Number

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,8 +1,21 @@
 /*
-    
+    what is recursion ?
+    -Any function which calls itself is call recursive.
+    -A recursive method solves a problem by calling a copy of itself to work on a 
+     smaller problem .this called recursion step..
+    - Write a iterative code
+
  */
 public class Recursion {
+    public static void printNo(int n){
+        if (n==0) {
+            return;
+        }
+        System.out.println(n);
+        printNo(n-1);;
+    }
     public static void main(String[] args) {
-        
+        int n =5;
+        printNo(n);
     }
 }
